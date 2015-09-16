5252a609b7680a193b80529b78f9cd591ba172f2
Sergi Maymi <smaymi.work@gmail.com>
2015-09-16 08:42:31 +0200

    styles and initial definition of an export file

--- client/src/app/company/companyListCtrl.js
+++ client/src/app/company/companyListCtrl.js
@@ -3,23 +3,28 @@
     angular
         .module('pcsManagement')
         .controller('CompanyListCtrl',
-            ['companyResource', '$scope',
+            ['companyResource', '$scope', '$timeout',
                 CompanyListCtrl]);
 
-    function CompanyListCtrl(companyResource, $scope) {
+    function CompanyListCtrl(companyResource, $scope, $timeout) {
         var vm = this;
 
+        vm.search = "";
 
         vm.findCompanies = function () {
             console.log('findCompanies');
             companyResource.query(function (data) {
                 vm.gridOptions.data = data;
+                if (data.length > 0) {
+                    vm.gridApi.grid.modifyRows(vm.gridOptions.data);
+                    vm.gridApi.selection.selectRow(vm.gridOptions.data[0]);
+                }
             });
         }
 
 
         vm.showSelected = function () {
-            console.log('getCurrentSelection code: '+vm.selectedCompany.code);
+            console.log('getCurrentSelection code: ' + vm.selectedCompany.code);
         }
 
 
@@ -37,22 +42,29 @@
                 var msg = 'row selected ' + row.isSelected;
                 console.log(msg);
                 vm.selectedCompany = row.entity;
+                console.log('getCurrentSelection code: ' + vm.selectedCompany.code);
+
             });
 
             gridApi.selection.on.rowSelectionChangedBatch($scope, function (rows) {
                 var msg = 'rows changed ' + rows.length;
                 console.log(msg);
- 
+
             });
-        };        
+        };
         //vm.gridOptions.data = 'vm.companies';
         vm.gridOptions.columnDefs = [
             { field: '_id', displayName: 'id' },
             { field: 'name' },
-            { field: 'code' },
-            { field: 'situation' },
-            { field: 'last_modification', cellFilter: 'date:"dd/MM/yyyy HH:mm"', type: 'date' }
+            { field: 'code', width: 100 },
+            { field: 'situation', width: 100 },
+            { field: 'last_modification', cellFilter: 'date:"dd/MM/yyyy HH:mm"', type: 'date', width: 150 }
         ];
 
+
+
     }
+
+
+
 } ());
